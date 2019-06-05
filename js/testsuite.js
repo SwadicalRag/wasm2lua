58311251cf9d2bee9e14f2ad05f22c3f6b7294b1
swadical <swadical@lab.glua.team>
2019-06-05 21:08:14 +0930

    use `luajit`, not `nilajit`

--- js/testsuite.js
+++ js/testsuite.js
@@ -19,7 +19,7 @@ const ignoredTests = [
     "math.untouched.wat.wasm",
     "string.optimized.wat.wasm",
 ];
-const LUA_PROGRAM = "nilajit";
+const LUA_PROGRAM = "luajit";
 let files = fs.readdirSync(__dirname + "/../resources/tests/c-testsuite/");
 for (let fileName of files) {
     if (fileName.match(/\.c$/)) {
