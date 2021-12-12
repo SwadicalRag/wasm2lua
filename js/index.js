e563e49cdc75c8462899a75acac1c0d455b4af3a
swadical <10724598+SwadicalRag@users.noreply.github.com>
2021-12-12 11:16:28 +1030

    Zero initialize locals that are not parameters as per WASM spec. Resolves #33

--- js/index.js
+++ js/index.js
@@ -2138,6 +2138,21 @@ class wasm2lua extends stringcompiler_1.StringCompiler {
             if (t_buf.pop() !== ",") {
                 return "";
             }
+            else {
+                this.write(t_buf, " = ");
+                seen = {};
+                for (let i = (state.funcType ? state.funcType.params.length : 0); i < state.regManager.registerCache.length; i++) {
+                    let reg = state.regManager.registerCache[i];
+                    let name = state.regManager.getPhysicalRegisterName(reg);
+                    if (seen[name]) {
+                        continue;
+                    }
+                    seen[name] = true;
+                    this.write(t_buf, "0");
+                    this.write(t_buf, ",");
+                }
+                t_buf.pop();
+            }
             this.write(t_buf, ";");
             this.newLine(t_buf);
             return t_buf.join("");
@@ -2146,7 +2161,7 @@ class wasm2lua extends stringcompiler_1.StringCompiler {
             if (state.regManager.totalRegisters > virtualregistermanager_1.VirtualRegisterManager.MAX_REG) {
                 this.write(t_buf, "local vreg = {");
                 for (let i = virtualregistermanager_1.VirtualRegisterManager.MAX_REG; i < state.regManager.totalRegisters; i++) {
-                    this.write(t_buf, `nil,`);
+                    this.write(t_buf, `0,`);
                 }
                 this.writeLn(t_buf, "}");
             }
@@ -2171,6 +2186,21 @@ class wasm2lua extends stringcompiler_1.StringCompiler {
                 }
                 else {
                     console.log(`WARNING: [${state.id}] ${state.regManager.totalRegisters} REGISTERS USED`);
+                }
+            }
+            this.write(t_buf, " = ");
+            for (let i = (state.funcType ? state.funcType.params.length : 0); i < state.regManager.totalRegisters; i++) {
+                if (i >= virtualregistermanager_1.VirtualRegisterManager.MAX_REG) {
+                    if (t_buf[t_buf.length - 1] == ",") {
+                        t_buf.pop();
+                    }
+                    break;
+                }
+                else {
+                    this.write(t_buf, "0");
+                    if (i !== (state.regManager.totalRegisters - 1)) {
+                        this.write(t_buf, ",");
+                    }
                 }
             }
             this.write(t_buf, ";");
